c8889c3ce383944e5223529ff22889fe2f3ad6b7
Andy Hayes <andygoestohollywood@gmail.com>
2015-10-25 13:51:39 +0200

    uglified build and available via localhost:3099/min

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,141 +1,162 @@
 /*global module:false*/
 module.exports = function (grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		// Metadata.
-		pkg: grunt.file.readJSON('package.json'),
-		project: {
-			src: 'src/app'
-			, js: '<%= project.src %>/**.*.js'
-			, dest: 'public/javascripts/build/'
-			, bundle: 'public/javascripts/build/thrust-engine.min.js'
-		},
-		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-		'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-		'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-		'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-		' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-		// Task configuration.
-		uglify: {
-			options: {
-				banner: '<%= banner %>'
-			},
-			dist: {
-				src: '<%= concat.dist.dest %>',
-				dest: 'dist/<%= pkg.name %>.min.js'
-			}
-		},
-		jshint: {
-			options: {
-				curly: true,
-				eqeqeq: true,
-				immed: true,
-				latedef: true,
-				newcap: true,
-				noarg: true,
-				sub: true,
-				undef: true,
-				unused: true,
-				boss: true,
-				eqnull: true,
-				browser: true,
-				globals: {}
-			},
-			gruntfile: {
-				src: 'Gruntfile.js'
-			},
-			lib_test: {
-				src: ['lib/**/*.js', 'test/**/*.js']
-			}
-		},
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    project: {
+      src: 'src/app',
+      js: '<%= project.src %>/**.*.js',
+      dest: './public/javascripts/browserify',
+      bundle: '<%= project.dest %>/thrust-engine.js'
+    },
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+    ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    uglify: {
+      options: {
+        compress: {
+          drop_console: true
+        },
+        sourceMap: true,
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= project.bundle %>',
+        dest: '<%= project.dest %>/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
 
-		browserify: {
-			test: {
-				src: ['test/tests.js'],
-				dest: './public/javascripts/test/tests.js',
-				options: {
-					watch: true,
-					transform: ['browserify-shim']
-				}
-			},
-			app: {
-				src: ['src/app/game.js'],
-				dest: './public/javascripts/browserify/thrust-engine.js',
-				options: {
-					keepAlive: true,
-					transform: ['browserify-shim'],
-					watch: true,
-					postBundleCB: function(err, src, next) {
-						grunt.log.writeln('bundle created successfully at: ' + new Date());
-						next(err, src);
-					},
-					browserifyOptions: {
-						debug: true
-					}
-				}
-			}
-		},
+    browserify: {
+      test: {
+        src: ['./src/test/tests.js'],
+        dest: './public/javascripts/test/tests.js',
+        options: {
+          watch: true,
+          transform: ['browserify-shim']
+        }
+      },
+      app: {
+        src: ['<%= project.src %>/game.js'],
+        dest: '<%= project.bundle %>',
+        options: {
+          keepAlive: true,
+          transform: ['browserify-shim'],
+          watch: true,
+          postBundleCB: function (err, src, next) {
+            grunt.log.writeln('bundle created successfully at: ' + new Date());
+            next(err, src);
+          },
+          browserifyOptions: {
+            debug: true
+          }
+        }
+      },
+      prod: {
+        src: ['<%= project.src %>/game.js'],
+        dest: '<%= project.bundle %>',
+        options: {
+          keepAlive: false,
+          transform: ['browserify-shim'],
+          watch: false,
+          postBundleCB: function (err, src, next) {
+            grunt.log.writeln('bundle created successfully at: ' + new Date());
+            next(err, src);
+          },
+          browserifyOptions: {
+            debug: true
+          }
+        }
+      }
+    },
 
-		mocha_phantomjs: {
-			all: ['./public/javascripts/test/*.html'],
-			options: {
-				reporter: 'html',
-				file: './coverage/test.html',
-				growlOnSuccess: false
-			}
-		},
+    mocha_phantomjs: {
+      all: ['./public/javascripts/test/*.html'],
+      options: {
+        reporter: 'html',
+        file: './coverage/test.html',
+        growlOnSuccess: false
+      }
+    },
 
-		smoothie: {
-			default_task: {
-				options: {
-					prompt: true,
-					src: "src/app/",
-					test: "test/",
-					moduleTemplate: './node_modules/grunt-smoothie/tasks/flavours/node/CustomType.js',
-					specTemplate: './node_modules/grunt-smoothie/tasks/flavours/node/CustomTypeSpec.js',
-					packageMap: [
-						{
-							name: 'Top Level',
-							value: ''
-						},
-						'actors',
-						'environment',
-						'states',
-						'utils'
-					]
-				}
-			}
-		},
+    smoothie: {
+      default_task: {
+        options: {
+          prompt: true,
+          src: "src/app/",
+          test: "test/",
+          moduleTemplate: './node_modules/grunt-smoothie/tasks/flavours/node/CustomType.js',
+          specTemplate: './node_modules/grunt-smoothie/tasks/flavours/node/CustomTypeSpec.js',
+          packageMap: [
+            {
+              name: 'Top Level',
+              value: ''
+            },
+            'actors',
+            'environment',
+            'states',
+            'utils'
+          ]
+        }
+      }
+    },
 
-		yuidoc: {
-			compile: {
-				name: '<%= pkg.name %>',
-				description: '<%= pkg.description %>',
-				version: '<%= pkg.version %>',
-				url: '<%= pkg.homepage %>',
-				options: {
-					paths: ['./src/app'],
-					outdir: './public/yuidoc'
-				}
-			}
-		}
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: ['./src/app'],
+          outdir: './public/yuidoc'
+        }
+      }
+    }
 
-	});
+  });
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-smoothie');
-	grunt.loadNpmTasks('grunt-mocha-phantomjs');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-browserify');
-	grunt.loadNpmTasks('grunt-contrib-requirejs');
-	grunt.loadNpmTasks('grunt-mocha-istanbul');
-	grunt.loadNpmTasks('grunt-contrib-yuidoc');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-smoothie');
+  grunt.loadNpmTasks('grunt-mocha-phantomjs');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
 
-	// Default task.
-	grunt.registerTask('default', ['browserify']);
-	grunt.registerTask('add', ['smoothie']);
+  // Default task.
+  grunt.registerTask('default', ['browserify:test', 'browserify:app']);
+  grunt.registerTask('add', ['smoothie']);
+  grunt.registerTask('build', ['browserify:prod', 'uglify']);
 
 };
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Thrust Engine' });
+  res.render('index', {
+    title: 'Thrust Engine',
+    engine: 'javascripts/browserify/thrust-engine.js'
+  });
+});
+
+router.get('/min', function(req, res, next) {
+  res.render('index', {
+    title: 'Thrust Engine',
+    engine: 'javascripts/browserify/thrust-engine.min.js'
+  });
 });
 
 module.exports = router;
