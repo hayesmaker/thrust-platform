8f59f4e180e7eb8f5652203d42b5d2a7496a01ec
Andy Hayes <andygoestohollywood@gmail.com>
2015-10-27 01:38:44 +0200

    home page versioninng

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+var pkg = require('../package');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-    title: 'Thrust Engine'
+    title: 'Thrust Engine',
+    version: pkg.version
   });
 });
 
