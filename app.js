cab6cedc6c9fbac4ca96dec71aa45c89d4edfea3
Andy Hayes <andygoestohollywood@gmail.com>
2015-10-26 12:35:24 +0200

    commenting serveIndex for now

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var serveIndex = require('serve-index');
+//var serveIndex = require('serve-index');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.use('/', serveIndex('public', {'icons': true}))
+//app.use('/', serveIndex('public', {'icons': true}))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
