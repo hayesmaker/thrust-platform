5f46cc868ac36eac8ccbe08c3fb16f1a866ee3de
hayesmaker <andygoestohollywood@gmail.com>
2016-02-23 11:33:48 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-    title: 'Thrust Engine',
+    title: pkg.title,
     version: pkg.version,
     engine: 'javascripts/browserify/thrust-engine.js'
   });
