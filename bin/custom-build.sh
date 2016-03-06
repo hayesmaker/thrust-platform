491259afa99a3f56624d5543d787c98dff4dc00f
Andy Hayes <andygoestohollywood@gmail.com>
2016-03-06 18:08:31 +0200

    asset pack bundling

--- bin/custom-build.sh
+++ bin/custom-build.sh
@@ -4,10 +4,10 @@ set -o errexit #Exit on error
 echo Custom Build env: $NODE_ENV
 
 if [[ $NODE_ENV == "production" ]]; then
-  echo "Production build"
-
+  echo "Production build - Bundling full game assets"
+  curl $THRUST_ASSETS_PACK
+  tar -xzf assets.tar.gz
 else
-  echo "Non production build"
-
+  echo "Non production build - Bundling demo assets"
 fi
-curl $THRUST_ASSETS_PACK
+npm run build
