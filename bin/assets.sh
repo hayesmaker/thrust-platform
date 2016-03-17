6922a8030ceb42dfffae0ccae2ffbd2fd95138ab
Andy Hayes <andygoestohollywood@gmail.com>
2016-03-17 10:36:28 +0200

    fixed asset cp script path

--- bin/assets.sh
+++ bin/assets.sh
@@ -4,4 +4,4 @@ set -o errexit #Exit on error
 echo Copying new assets
 
 cp ./src/app/properties.js $THRUST_ASSETS_PATH/src/app/
-cp -a ./public/assets $THRUST_ASSETS_PATH/public/assets
+cp -a ./public/assets $THRUST_ASSETS_PATH/public/
