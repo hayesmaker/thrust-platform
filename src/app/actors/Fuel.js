344f51a8549689acc6375acbd34638612a7b36c3
hayesmaker <andygoestohollywood@gmail.com>
2016-04-08 10:58:17 +0100

    Fuel explode system && refuelling added

--- src/app/actors/Fuel.js
+++ src/app/actors/Fuel.js
@@ -1,26 +1,31 @@
 var PhysicsActor = require('./PhysicsActor');
 var FuelParticlesSystem = require('./FuelParticlesSystem');
 var utils = require('../utils');
+var gameState = require('../data/game-state');
+var _ = require('lodash');
+var particles = require('../environment/particles');
 
 /**
  * Fuel Sprite - PhysicsActor enabled fuel cell sprite
  *
  * @class Fuel
- * @param {Collisions} collisions - Our collisions container of collisionGroups.
- * @param {Groups} groups - Our groups container.
+ * @param {Collisions} collisions - Our collisions groups.
+ * @param {Groups} groups - Our display groups.
  * @param {String} imageCacheKey - Sprite image key.
  * @param {Number} [x] - initial position x, if unset is 0
  * @param {Number} [y] - initial position y, if unset is 0
- * @extends {Phaser.Sprite}
+ * @extends {PhysicsActor}
  * @constructor
  */
 function Fuel (collisions, groups, imageCacheKey, x, y) {
   PhysicsActor.call(this, collisions, groups, imageCacheKey, x, y);
+  this.health = 250;
 }
 
 var p = Fuel.prototype = Object.create(PhysicsActor.prototype, {
   constructor: Fuel
 });
+
 module.exports = Fuel;
 
 /**
@@ -36,13 +41,22 @@ p.player = null;
 p.particles = null;
 
 /**
+ *
+ * @property refuelAmount
+ * @type {number}
+ */
+p.refuelAmount= 1;
+
+/**
  * @method init
  */
 p.init = function() {
-  console.log('Fuel :: init ::', this);
   this.createParticles();
 };
 
+/**
+ * @method update
+ */
 p.update = function() {
   this.checkPlayerVicinity();
 };
@@ -52,16 +66,49 @@ p.createParticles = function() {
   this.particles.init(this.position);
 };
 
+/**
+ * Destroys this Fuel game object
+ *
+ * @method kill
+ */
+p.kill = function() {
+  this.alive = false;
+  gameState.score += gameState.SCORES.FUEL;
+  TweenLite.to(this, 0.3, {alpha: 0, ease: Quad.easeOut, onComplete:_.bind(this.onFadeComplete, this)});
+};
+
+/**
+ * When Fuel is killed or destroyed it is removed from the game
+ *
+ * @method onFadeComplete
+ */
+p.onFadeComplete = function() {
+  console.log('onFadeComplete', this.alive);
+  particles.explode(this.x, this.y);
+  Phaser.Sprite.prototype.kill.call(this);
+
+};
+
+/**
+ * If a player is close to the fuel, refuelling can happen
+ * and particles emitted to show the refuel.
+ *
+ * @method checkPlayerVicinity
+ */
 p.checkPlayerVicinity = function() {
   var dist = utils.distAtoB(this.player.position, this.position);
-  if (dist < 80) {
+  if (this.alive && this.player.alive && dist < 80) {
     if (!this.particles.isEmitting) {
       this.particles.start(this.position, this.player.position);
+      TweenMax.to(this, 0.5, {tint:0xfffffe, tintAmount:1});
     }
     this.particles.update();
+    gameState.fuel+=this.refuelAmount;
+    this.damage(1);
   } else {
     if (this.particles.isEmitting) {
       this.particles.stop();
+      this.tint = 0xffffff;
     }
   }
 };
--- src/app/data/game-state.js
+++ src/app/data/game-state.js
@@ -1,11 +1,23 @@
 /**
  * keeps a record of current game state data
- * 
+ * defines score and bonuses
+ *  
  * @class game-state
- * @type {{initialise: function, score: number, fuel: number, lives: number}}
+ * @type {{SCORES: {FUEL: number, LIMPET: number, PLANET_BUSTER: number, ORB_RECOVERED: number, LIMPETS_DESTROYED: number}, initialise: module.exports.initialise, score: number, fuel: number, lives: number}}
  * @static
  */
 module.exports = {
+
+  /**
+   * @property SCORES
+   */
+  SCORES: {
+    FUEL: 100,
+    LIMPET: 200,
+    PLANET_BUSTER: 1000,
+    ORB_RECOVERED: 750,
+    LIMPETS_DESTROYED: 500
+  },
   
   initialise: function() {
     this.score = 5;
