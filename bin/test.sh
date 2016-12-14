63c800a3b0107c75fe50d007912a4f39257d7e90
hayesmaker <andygoestohollywood@gmail.com>
2016-12-14 10:28:55 +0000

    travis buld fix try 2

--- bin/test.sh
+++ bin/test.sh
@@ -5,9 +5,9 @@ echo Test Sweep env: $NODE_ENV
 npm run mocha
 
 if [[ $NODE_ENV == "travis" ]]; then
-  echo "Skipping Travis build: Launch e2e tests on browserstack"
-  #npm run e2e:travis
+  echo "Travis build: Launch e2e tests on browserstack"
+  npm run e2e:travis
 else
-  echo "Skipping Local E2E testing"
+  echo "Local ENV Detected - Skipping E2E testing"
   #npm run nightwatch
 fi
