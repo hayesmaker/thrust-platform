f1ff3c837759223110b3c2d2718a854547d2c8e0
hayesmaker <andygoestohollywood@gmail.com>
2020-09-06 16:47:58 +0100

    Build crap

--- bin/custom-build.sh
+++ bin/custom-build.sh
@@ -7,10 +7,10 @@ if [[ $NODE_ENV == "production" ]]; then
   echo "Production build - Bundling full game assets"
   curl -O $THRUST_ASSETS_PACK
   tar -xzf assets.tar.gz
+  npm run build:heroku
 else
   echo "Non production build - Bundling demo assets"
 fi
-npm run build:heroku
 
 #  copy and paste for local levels
 # curl -O $THRUST_ASSETS_PACK && tar -xzf assets.tar.gz
