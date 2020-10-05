a7d6bdf5be41b54389c02924e59ad721bad8f6e6
hayesmaker <andygoestohollywood@gmail.com>
2020-10-05 16:01:03 +0100

    Reorganising builds and adding variations back to support page

--- src/old/main-webgl.js
+++ src/old/main-webgl.js
@@ -1,7 +1,17 @@
 var launcher = require('./states/launcher');
+var version = require('./package.json').version;
 
 window.onload = function() {
   launcher.renderMode = Phaser.WEBGL;
   launcher.enableHiResMode();
+  launcher.setCustomOptions({
+    options: {
+      version: version,
+      versionSuffix: 'wgl',
+      gameModes: {
+        allLevels: true
+      }
+    }
+  });
   launcher.start();
 };
